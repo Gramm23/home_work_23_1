9b0aa87c9d17fb97152d777306371f3d78d98dea
Sergey <seglapinofff@gmail.com>
2023-12-17 00:22:41 +0300

    init

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import authenticate, login
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
